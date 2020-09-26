799c698bb8cf4674fa2f3b958473fba621594a5f
Himanshu <himanshusogarwal@gmail.com>
2020-09-26 22:31:37 +0530

    login page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".login-btn").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="flex";
+})
+var close=document.querySelector(".close-button").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+})
+
